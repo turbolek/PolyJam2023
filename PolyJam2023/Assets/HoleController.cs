a7c193e6f61238cd9deb7bcc3d8b59fcc8c5dec3
turbolek <dankunicki@gmail.com>
2023-02-04 20:02:38 +0100

    Fixed holes

--- PolyJam2023/Assets/HoleController.cs
+++ PolyJam2023/Assets/HoleController.cs
@@ -4,8 +4,12 @@ using UnityEngine;
 
 public class HoleController : MonoBehaviour
 {
-    [SerializeField]
     private GameplayManager _gameplayManager;
+
+    private void Start()
+    {
+        _gameplayManager = FindObjectOfType<GameplayManager>();
+    }
 
     private void OnTriggerEnter2D(Collider2D collision)
     {
